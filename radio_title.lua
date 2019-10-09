c85e531bc05d9cdd05bd203662c728926e3eb42f
Olivier Le Moal <olivier.lemoal@p1sec.com>
2019-10-10 00:30:50 +0200

    first commit

---
+++ radio_title.lua
@@ -0,0 +1,49 @@
+--[[
+  Get title for current song from external source
+]]--
+
+local settings = {
+    radio_title_helper = "/usr/local/bin/radio_title_helper", -- Path to Radio title helper
+    interval = 10, -- Renew every 10 seconds (default)
+    radios = {"fip", "franceculture", "francemusique", "tsfjazz", "franceinter"}, -- enable script for these radios (keyword must be in stream url)
+}
+
+require 'mp.options'
+read_options(settings, "radiotitle")
+local msg = require 'mp.msg'
+local utils = require 'mp.utils'
+
+function get_song_name()
+  local radio_url = mp.get_property('stream-open-filename')
+  t = {}  
+  t.args = {settings.radio_title_helper, radio_url}
+  res = utils.subprocess(t)
+  if res.status == 0 then
+      return res.stdout
+  else
+      mp.msg.error("Could not retrieve title")
+  end
+end
+
+function change_title()
+  local title = mp.get_property('media-title')
+  local new_title = get_song_name()
+  if new_title and title ~= new_title then 
+    mp.set_property("file-local-options/force-media-title", new_title)
+    msg.info("Now playing "..new_title)
+  end
+  mp.add_timeout(settings.interval, change_title)
+end
+
+
+function enable_script()
+  local radio_url = mp.get_property('stream-open-filename')
+  for index, radio in ipairs(settings.radios) do
+      if string.find(radio_url, radio) then
+        change_title()
+      end
+  end
+end
+
+-- Wait 5s for mpv to open stream
+mp.add_timeout(5, enable_script)
